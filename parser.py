9d4f751f47e97163ba3ea55c6001934e5e1911e8
Alexandra Shkorinova <a.shkorinova@gmail.com>
2017-11-03 14:51:54 +0200

    Minor fixes

--- parser.py
+++ parser.py
@@ -2,11 +2,13 @@ import re
 pattern = re.compile("\s*((?:\w(?!\s+')+|\/+|\s(?!\s*'))+\w)\s*")
 tickers = []
 
-for i, line in enumerate(open('./your_file_name.txt', 'r')):
+for i, line in enumerate(open('./SnP500 Securities in Array by Year.txt', 'r')):
     for match in re.finditer(pattern, line):
         tickers.append(match.group())
-unique_tickers = list(set(tickers))
-unique_securities = [ticker + ' Equity' for ticker in unique_tickers]
+#unique_tickers = list(set(tickers))
+tickers = list(tickers)
+securities = [ticker[:-2] + 'US Equity' for ticker in tickers]
+unique_securities = set(securities)
 
-with open('./output.txt', "w+") as output:
-    output.write(", ".join(map(str, unique_securities)))
+with open('./output1.txt', "w+") as output:
+    output.write(",\n".join(map(str, unique_securities)))
